89053889f83b20fade1771607bccba5abd45b3e8
Roberto Zaratin <roberto.zaratin@smc.it>
2025-01-24 20:07:26 +0100

    update python-dev

--- install/10-prepare-raspi.sh
+++ install/10-prepare-raspi.sh
@@ -16,5 +16,5 @@ sudo apt-get upgrade
 echo "Install + build led drivers"
 sudo apt-get -y install git vim python3-pip python3-rpi.gpio gcc make build-essential
 sudo apt-get -y install libatlas-base-dev 
-sudo apt-get -y install python-dev swig scons # for building WS2811 drivers
+sudo apt-get -y install python3-dev swig scons # for building WS2811 drivers
 
