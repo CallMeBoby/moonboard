7456f70e951d9ac809c855a04d21d8ed35327ffa
Roberto Zaratin <roberto.zaratin@smc.it>
2025-02-04 01:54:17 +0100

    clear code

--- ble/moonboard_ble_service.py
+++ ble/moonboard_ble_service.py
@@ -1,48 +1,12 @@
-import sys
-from gatt_base.gatt_lib_advertisement import Advertisement
-from gatt_base.gatt_lib_characteristic import Characteristic
-from gatt_base.gatt_lib_service import Service
-import string,json
+
+import json
 import subprocess
 import logging
 from moonboard_app_protocol import UnstuffSequence, decode_problem_string
 import paho.mqtt.client as mqtt
 
 import os
-import threading
 import pty
- 
-# FIXME: remove dead code
-# FIXME: cleanup code & simplify(!)
-
-BLUEZ_SERVICE_NAME =           'org.bluez'
-DBUS_OM_IFACE =                'org.freedesktop.DBus.ObjectManager'
-LE_ADVERTISING_MANAGER_IFACE = 'org.bluez.LEAdvertisingManager1'
-GATT_MANAGER_IFACE =           'org.bluez.GattManager1'
-GATT_CHRC_IFACE =              'org.bluez.GattCharacteristic1'
-UART_SERVICE_UUID =            '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
-UART_RX_CHARACTERISTIC_UUID =  '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
-UART_TX_CHARACTERISTIC_UUID =  '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
-LOCAL_NAME =                   'Moonboard A'
-SERVICE_NAME=                  'com.moonboard'
-
-# FIXME: on connection lost: error: UnicodeDecodeError: 'utf-8' codec can't decode byte 0xff in position 2: invalid start byte
-
-class RxCharacteristic(Characteristic):
-    def __init__(self, bus, index, service, process_rx):
-        Characteristic.__init__(self, bus, index, UART_RX_CHARACTERISTIC_UUID,
-                                ['write'], service)
-        self.process_rx=process_rx
-
-    def WriteValue(self, value, options):
-        pass
-        #self.process_rx(value)
-
-class UartService(Service):
-    def __init__(self, bus,path, index, process_rx):
-        Service.__init__(self, bus,path, index, UART_SERVICE_UUID, True)
-        self.add_characteristic(RxCharacteristic(bus, 1, self, process_rx))   
-
 
 class OutStream: # FIXME: simplify
     def __init__(self, fileno):
