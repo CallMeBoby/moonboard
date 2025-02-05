4ee9a30403202a054948ba6085113417f8a238ac
Roberto Zaratin <roberto.zaratin@smc.it>
2025-02-05 12:51:58 +0100

    command restart service

--- scripts/fix_startup.sh
+++ scripts/fix_startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo systemctl restart moonboard_led.service
-sudo systemctl restart moonboard_ble.service
+#sudo systemctl restart moonboard_ble.service
+sudo systemctl restart com.moonboard.service
 
